d1837a65f5016cc6e41237040fbf7c6354d65bd6
Nikhil Gupta <mywork.ng@gmail.com>
2020-03-11 21:36:15 -0500

    added forecast function to model compare class

--- Projects/gdp_prediction/ModelCompareUnivariate.R
+++ Projects/gdp_prediction/ModelCompareUnivariate.R
@@ -24,7 +24,7 @@ ModelCompareUnivariate = R6Class(
       self$add_models(mdl_list)
       private$set_n.ahead(n.ahead)
       private$set_batch_size(batch_size)
-      self$compute_metrics()
+      self$compute_metrics(step_n.ahead = TRUE)
       
     },
     
@@ -32,6 +32,9 @@ ModelCompareUnivariate = R6Class(
     
     get_x = function(){return(self$x)},
     set_x = function(x){self$x = x},
+    get_len_x = function(){
+      return(length(self$get_x()))
+    },
     
     get_batch_size = function(){return(self$batch_size)},
     get_n.ahead = function(){return(self$n.ahead)},
@@ -67,23 +70,33 @@ ModelCompareUnivariate = R6Class(
     },
     
     #### General Public Methods ----
-    compute_metrics = function(){
+    compute_metrics = function(step_n.ahead = FALSE){
       for (name in names(self$get_models())){
         
         if (self$models[[name]][['metric_has_been_computed']] == FALSE){
+          cat(paste("Computing metrics for: ", name, "\n"))
           
-          res = sliding_ase(x = self$get_x(),
-                            phi = self$get_models()[[name]][['phi']],
-                            theta = self$get_models()[[name]][['theta']],
-                            d = self$get_models()[[name]][['d']],
-                            s = self$get_models()[[name]][['s']],
-                            n.ahead = self$get_n.ahead(),
-                            batch_size = self$get_models()[[name]][['batch_size']])
+          res = private$sliding_ase(x = self$get_x(),
+                                    phi = self$get_models()[[name]][['phi']],
+                                    theta = self$get_models()[[name]][['theta']],
+                                    d = self$get_models()[[name]][['d']],
+                                    s = self$get_models()[[name]][['s']],
+                                    n.ahead = self$get_n.ahead(),
+                                    batch_size = self$get_models()[[name]][['batch_size']],
+                                    step_n.ahead = step_n.ahead)
           
+
           ## Inplace
           self$models[[name]][['ASEs']] = res$ASEs  
-          self$models[[name]][['Time']] = res$Time
+          self$models[[name]][['time.rolling.ase']] = res$time.rolling.ase
+          self$models[[name]][['f']] = res$f
+          self$models[[name]][['ll']] = res$ll
+          self$models[[name]][['ul']] = res$ul
+          self$models[[name]][['time.forecasts']] = res$time.forecasts
+          
+          
           self$models[[name]][['metric_has_been_computed']] = TRUE
+          
         }
         else{
           warning(paste("Metrics have already been computed for Model: '", name, "'. These will not be computed again."))
@@ -91,27 +104,107 @@ ModelCompareUnivariate = R6Class(
       }
     },
     
-    compare_forecasts = function(){
-      # Visual Comparison of future forecasts
+    plot_histogram_ases = function(){
+      results = self$get_tabular_metrics()
+      print(ggplot(results, aes(x = Model, y = ASE, color = Model)) + geom_boxplot() + coord_flip())
+    },
+    
+    plot_forecasts = function(only_sliding = TRUE){
+      results.forecasts = self$get_tabular_metrics(ases = FALSE)
+      
+      model_subset = c("Realization")
+      if (only_sliding){
+        for (name in names(self$get_models())){
+          if (self$models[[name]][['sliding_ase']] == TRUE){
+            model_subset = c(model_subset, name)
+          }
+        }
+      }
+      else{
+        # Add all models
+        for (name in names(self$get_models())){
+          model_subset = c(model_subset, name)
+        }
+      }
+
+      
+      results.forecasts = results.forecasts %>% 
+        filter(Model %in% model_subset)
+      
+      rects = data.frame(xstart = seq(49,133,12), xend = seq(60,144,12), Batch = rep(1, 8))
+      p = ggplot() + 
+        geom_rect(data = rects, aes(xmin = xstart, xmax = xend, ymin = -Inf, ymax = Inf, fill = Batch), alpha = 0.1, show.legend = FALSE) +  
+        geom_line(results.forecasts, mapping = aes(x = Time, y = f, color = Model))  
+        
+        
+      
+      print(p) 
+    },
+    
+    statistical_compare = function(){
+      results = self$get_tabular_metrics(only_sliding = TRUE)
+      
+      # Analysis of Variance
+      res.aov = aov(ASE ~ Model, data = results)
+      print(summary(res.aov))
+      cat("\n\n")
+      
+      # Tukey Adjustment
+      print(TukeyHSD(res.aov))
+      
+      return(res.aov)
       
     },
     
-    plot_histogram_ases = function(){
-      # Visual Comparison of future forecasts
-      results = tribble(~Model, ~ASE) 
+    get_tabular_metrics = function(only_sliding = FALSE, ases = TRUE){
+      # ases = TRUE returns the ASE values for each batch
+      # ases = FALSE returns the forecasts, and the lower limits and upper limits asscoiated with the forecasts
+      
+      if (ases == TRUE){
+        results = tribble(~Model, ~ASE, ~Time) 
+      }
+      else {
+        results = tribble(~Model, ~Time, ~f, ~ll, ~ul) 
+        #results = tribble(~Model, ~f, ~ll, ~ul) 
+      }
+      
       for (name in names(self$get_models())){
         if (self$models[[name]][['metric_has_been_computed']] == TRUE){
-          results = results %>% add_row(Model = name, ASE = self$models[[name]][['ASEs']])   
+          
+          if (ases == TRUE){
+            results = results %>% add_row(Model = name,
+                                          ASE = self$models[[name]][['ASEs']],
+                                          Time = self$models[[name]][['time.rolling.ase']])
+          }
+          else{
+            results = results %>% add_row(Model = name,
+                                          Time = self$models[[name]][['time.forecasts']],
+                                          f = self$models[[name]][['f']],
+                                          ll = self$models[[name]][['ll']],
+                                          ul = self$models[[name]][['ul']])
+
+          }
         }
         else{
           warning(paste("Metrics have not been computed for Model: '", name, "'. These will not be plotted."))
         }
       }
       
-      print(ggplot(results, aes(x = Model, y = ASE, color = Model)) + geom_boxplot() + coord_flip())
+      if (ases == FALSE){
+        # Add the realization as well
+        results = results %>% add_row(Model = "Realization",
+                                      Time = seq(1, self$get_len_x(), 1),
+                                      f = self$get_x(),
+                                      ll = rep(NA, self$get_len_x()),
+                                      ul = rep(NA, self$get_len_x()))
+      }
+      
+      return(results)
     }
+  
     
   ),
+    
   
   #### Private Methods ----
   private = list(
@@ -171,6 +264,123 @@ ModelCompareUnivariate = R6Class(
       }
       self$batch_size = batch_size
       private$set_batch_per_model()
+    },
+    
+    sliding_ase = function(x,
+                           phi = 0, theta = 0, d = 0, s = 0, # ARUMA arguments
+                           linear = NA, freq = NA,           # Signal + Noise arguments
+                           n.ahead = NA, batch_size = NA,    # Forecasting specific arguments
+                           time_index = 'test_start',        # Time Index to return ("<test|data>_<start|end>") 
+                           step_n.ahead = FALSE,
+                           ...)                              # max.p (sigplusnoise), lambda (ARUMA)      
+    {
+      # Sliding CV ... batches are mutually exclusive
+      
+      n = length(x)
+      
+      if (is.na(batch_size)){
+        warning("Batch Size has not been specified. Will assume a single batch")
+        cat("\n")
+        batch_size = n
+      }
+      
+      if (is.na(n.ahead)){
+        stop("Number of points to be used for forecasting has not been specified. Please specify n.ahead")
+      }
+      
+      if (all(phi == 0) & all(theta == 0) & d == 0 & s == 0){
+        if (is.na(linear) & is.na(freq)){
+          stop("You have specified the arguments for neither an ARMA/ARUMA model or a Signal + Noise Model. Please specify at least one of these to continue")
+        }
+      }
+      
+      aruma = FALSE
+      if (!(all(phi == 0) & all(theta == 0) & d == 0 & s == 0)){
+        aruma = TRUE
+      }
+      else{
+        # Signal + Noise model
+      }
+      
+      forecasts.f = rep(NA, n)
+      forecasts.ul = rep(NA, n)
+      forecasts.ll = rep(NA, n)
+      time.forecasts = seq(1, n, 1)
+      
+      start = 1
+      
+      if (step_n.ahead == FALSE){
+        # Step Size = 1
+        step_size = 1
+        num_batches = n-batch_size+1  
+      }
+      else{
+        # Step by n.ahead each time
+        step_size = n.ahead
+        num_batches = floor((n-batch_size)/n.ahead)  + 1
+      }
+      
+      cat(paste("Number of batches expected: ", num_batches, "\n"))
+      
+      ASEs = numeric(num_batches)
+      time.rolling.ase = numeric(num_batches)
+      
+      for (i in 0:(num_batches-1))
+      {
+        # Define the batch
+        subset = x[start:(batch_size+i*step_size)]
+        # Take last n.ahead observations from the batch and use that to compare with the forecast
+        
+        test_start = i*step_size + batch_size - n.ahead + 1
+        test_end = i*step_size + batch_size
+        data_start = start
+        data_end = i*step_size + batch_size
+        
+        if (time_index == 'test_start'){
+          time.rolling.ase[i+1] = test_start
+        }
+        else if (time_index == 'test_end'){
+          time.rolling.ase[i+1] = test_end
+        }
+        else if (time_index == 'data_start'){
+          time.rolling.ase[i+1] = data_start
+        }
+        else if (time_index == 'data_end'){
+          time.rolling.ase[i+1] = data_end
+        }
+        else{
+          stop("You have not provided the correct time_index. Options are <data|test>_<start_end>")
+        }
+        
+        test_data = x[test_start:test_end]
+        
+        if (aruma){
+          forecasts = tswge::fore.aruma.wge(x = subset, phi = phi, theta = theta, d = d, s = s,
+                                            n.ahead = n.ahead, lastn = TRUE, plot = FALSE, ...)
+        }
+        else{
+          forecasts = tswge::fore.sigplusnoise.wge(x = subset, linear = linear, freq = freq,
+                                                   n.ahead = n.ahead, lastn = TRUE, plot = FALSE, ...)
+        }
+        
+        ASEs[i+1] = mean((test_data - forecasts$f)^2)
+        start = start + step_size
+        
+        forecasts.f[test_start: test_end] = forecasts$f 
+        forecasts.ll[test_start: test_end] = forecasts$ll
+        forecasts.ul[test_start: test_end] = forecasts$ul
+        time.forecasts[test_start: test_end] = seq(test_start, test_end, 1)
+      }
+      
+      # print("sliding_ase.4: forecasts")
+      # print(forecasts.f)
+      
+      return(list(ASEs = ASEs,
+                  time.rolling.ase = time.rolling.ase,
+                  f = forecasts.f,
+                  ll = forecasts.ll,
+                  ul = forecasts.ul,
+                  time.forecasts = time.forecasts ))
     }
     
     
--- Projects/gdp_prediction/ModelCompareUnivariate.md
+++ Projects/gdp_prediction/ModelCompareUnivariate.md
@@ -1,7 +1,7 @@
 Model Compare Univariate
 ================
 Nikhil Gupta
-2020-03-08 16:53:53
+2020-03-11 21:35:06
 
 ``` r
 library(tswge)
@@ -16,7 +16,7 @@ library(tidyverse)
 
     ## Warning: package 'tidyverse' was built under R version 3.5.3
 
-    ## -- Attaching packages -------------------------------------------------------------------------------------------- tidyverse 1.2.1 --
+    ## -- Attaching packages ----------------------------------------------------------------------------------------------------- tidyverse 1.2.1 --
 
     ## v ggplot2 3.2.0     v purrr   0.3.2
     ## v tibble  2.1.3     v dplyr   0.8.3
@@ -39,7 +39,7 @@ library(tidyverse)
 
     ## Warning: package 'forcats' was built under R version 3.5.3
 
-    ## -- Conflicts ----------------------------------------------------------------------------------------------- tidyverse_conflicts() --
+    ## -- Conflicts -------------------------------------------------------------------------------------------------------- tidyverse_conflicts() --
     ## x dplyr::filter() masks stats::filter()
     ## x dplyr::lag()    masks stats::lag()
 
@@ -59,14 +59,110 @@ models = list("ARMA Model 1A" = list(phi = c(0.5, 1), theta = c(0.5), sliding_as
 ```
 
 ``` r
-mdl_compare = ModelCompareUnivariate$new(x = airlog, mdl_list = models, n.ahead = 36, batch_size = 108)
+mdl_compare = ModelCompareUnivariate$new(x = airlog, mdl_list = models, n.ahead = 12, batch_size = 60)
 ```
 
-    ## Warning in sliding_ase(x = self$get_x(), phi = self$get_models()[[name]]
-    ## [["phi"]], : Batch Size has not been specified. Will assume a single batch
+    ## Computing metrics for:  ARMA Model 1A
+
+    ## Warning in private$sliding_ase(x = self$get_x(), phi = self$get_models()
+    ## [[name]][["phi"]], : Batch Size has not been specified. Will assume a
+    ## single batch
+
+    ## 
+    ## Number of batches expected:  1 
+    ## Computing metrics for:  ARMA Model 1B 
+    ## Number of batches expected:  8 
+    ## Computing metrics for:  ARMIA Model 2 
+    ## Number of batches expected:  8 
+    ## Computing metrics for:  ARMUA Model 3 
+    ## Number of batches expected:  8
 
 ``` r
 mdl_compare$plot_histogram_ases()
 ```
 
-![](ModelCompareUnivariate_files/figure-markdown_github/unnamed-chunk-4-1.png)
+![](ModelCompareUnivariate_files/figure-markdown_github/unnamed-chunk-5-1.png)
+
+``` r
+mdl_compare$plot_forecasts(only_sliding = TRUE)
+```
+
+![](ModelCompareUnivariate_files/figure-markdown_github/unnamed-chunk-6-1.png)
+
+``` r
+mdl_compare$statistical_compare()  
+```
+
+    ##             Df Sum Sq Mean Sq F value Pr(>F)    
+    ## Model        3  616.9  205.62   743.3 <2e-16 ***
+    ## Residuals   21    5.8    0.28                   
+    ## ---
+    ## Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+    ## 
+    ## 
+    ##   Tukey multiple comparisons of means
+    ##     95% family-wise confidence level
+    ## 
+    ## Fit: aov(formula = ASE ~ Model, data = results)
+    ## 
+    ## $Model
+    ##                                    diff         lwr         upr     p adj
+    ## ARMA Model 1B-ARMA Model 1A -25.3309119 -26.8858531 -23.7759707 0.0000000
+    ## ARMIA Model 2-ARMA Model 1A -25.4779990 -27.0329403 -23.9230578 0.0000000
+    ## ARMUA Model 3-ARMA Model 1A -25.2215646 -26.7765058 -23.6666234 0.0000000
+    ## ARMIA Model 2-ARMA Model 1B  -0.1470871  -0.8800935   0.5859192 0.9429453
+    ## ARMUA Model 3-ARMA Model 1B   0.1093473  -0.6236590   0.8423536 0.9751573
+    ## ARMUA Model 3-ARMIA Model 2   0.2564344  -0.4765719   0.9894408 0.7648553
+
+    ## Call:
+    ##    aov(formula = ASE ~ Model, data = results)
+    ## 
+    ## Terms:
+    ##                    Model Residuals
+    ## Sum of Squares  616.8658    5.8092
+    ## Deg. of Freedom        3        21
+    ## 
+    ## Residual standard error: 0.5259562
+    ## Estimated effects may be unbalanced
+
+``` r
+# Only compares models that have sliding_ase calculations, since we need more than 1 result per model
+```
+
+``` r
+mdl_compare$get_tabular_metrics(ases = TRUE)
+```
+
+    ## # A tibble: 25 x 3
+    ##    Model             ASE  Time
+    ##    <chr>           <dbl> <dbl>
+    ##  1 ARMA Model 1A 25.9      133
+    ##  2 ARMA Model 1B  1.82      49
+    ##  3 ARMA Model 1B  0.449     61
+    ##  4 ARMA Model 1B  0.0952    73
+    ##  5 ARMA Model 1B  0.635     85
+    ##  6 ARMA Model 1B  0.572     97
+    ##  7 ARMA Model 1B  0.868    109
+    ##  8 ARMA Model 1B  0.125    121
+    ##  9 ARMA Model 1B  0.127    133
+    ## 10 ARMIA Model 2  0.667     49
+    ## # ... with 15 more rows
+
+``` r
+mdl_compare$get_tabular_metrics(ases = FALSE)
+```
+
+    ## # A tibble: 720 x 5
+    ##    Model          Time     f    ll    ul
+    ##    <chr>         <dbl> <dbl> <dbl> <dbl>
+    ##  1 ARMA Model 1A     1    NA    NA    NA
+    ##  2 ARMA Model 1A     2    NA    NA    NA
+    ##  3 ARMA Model 1A     3    NA    NA    NA
+    ##  4 ARMA Model 1A     4    NA    NA    NA
+    ##  5 ARMA Model 1A     5    NA    NA    NA
+    ##  6 ARMA Model 1A     6    NA    NA    NA
+    ##  7 ARMA Model 1A     7    NA    NA    NA
+    ##  8 ARMA Model 1A     8    NA    NA    NA
+    ##  9 ARMA Model 1A     9    NA    NA    NA
+    ## 10 ARMA Model 1A    10    NA    NA    NA
+    ## # ... with 710 more rows
